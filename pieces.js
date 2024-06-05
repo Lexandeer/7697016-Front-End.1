2f761e55bebccd166ef3a65ee387ffb38d3259ae
Alexandre <alexmiquel888@gmail.com>
2024-06-05 11:12:08 +0200

    completed

--- pieces.js
+++ pieces.js
@@ -1,185 +1,179 @@
-// Récupération des pièces éventuellement stockées dans le localStorage
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis, afficherAvis, afficherGraphiqueAvis } from "./avis.js";
+//Récupération des pièces eventuellement stockées dans le localStorage
 let pieces = window.localStorage.getItem("pieces");
-    if(pieces === null){
-        // Récupération des pièces depuis l'API
-        const reponse = await fetch("http://localhost:8081/pieces/");
-        const pieces = await reponse.json();
 
-        // Transformation des pièces en JSON
-        const valeurPieces = JSON.stringify(pieces);
+if (pieces === null) {
+	// Récupération des pièces depuis l'API
+	const reponse = await fetch("http://localhost:8081/pieces/");
+	pieces = await reponse.json();
+	// Transformation des pièces en JSON
+	const valeurPieces = JSON.stringify(pieces);
+	// Stockage des informations dans le localStorage
+	window.localStorage.setItem("pieces", valeurPieces);
+} else {
+	pieces = JSON.parse(pieces);
+}
+// on appel la fonction pour ajouter le listener au formulaire
+ajoutListenerEnvoyerAvis();
 
-        // Stockage des informations dans le localStorage
-        window.localStorage.setItem("pieces", valeurPieces);
-    }else{
-        // Transformation des pièces en JSON
-        pieces = JSON.parse(pieces);
-    }
-
-import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
-
-
-//On appelle la fonction :
-ajoutListenerEnvoyerAvis()
-
-
-// Fonction qui génère toute la page web
 function genererPieces(pieces) {
-    for (let i = 0; i < pieces.length; i++){
-        
-        const article = pieces[i];
+	for (let i = 0; i < pieces.length; i++) {
 
-        // Récupération de l'élément du DOM qui accueillera les fiches
+		const article = pieces[i];
+		// Récupération de l'élément du DOM qui accueillera les fiches
 		const sectionFiches = document.querySelector(".fiches");
+		// Création d’une balise dédiée à une pièce automobile
+		const pieceElement = document.createElement("article");
+		pieceElement.dataset.id = pieces[i].id;
+		// Création des balises 
+		const imageElement = document.createElement("img");
+		imageElement.src = article.image;
+		const nomElement = document.createElement("h2");
+		nomElement.innerText = article.nom;
+		const prixElement = document.createElement("p");
+		prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+		const categorieElement = document.createElement("p");
+		categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+		const descriptionElement = document.createElement("p");
+		descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+		const stockElement = document.createElement("p");
+		stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+		//Code ajouté
+		const avisBouton = document.createElement("button");
+		avisBouton.dataset.id = article.id;
+		avisBouton.textContent = "Afficher les avis";
 
-        const pieceElement = document.createElement("article");
+		// On rattache la balise article a la section Fiches
+		sectionFiches.appendChild(pieceElement);
+		pieceElement.appendChild(imageElement);
+		pieceElement.appendChild(nomElement);
+		pieceElement.appendChild(prixElement);
+		pieceElement.appendChild(categorieElement);
+		pieceElement.appendChild(descriptionElement);
+		pieceElement.appendChild(stockElement);
+		//Code aJouté
+		pieceElement.appendChild(avisBouton);
 
-        // On crée l’élément img.
-        const imageElement = document.createElement("img");
-        // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
-        imageElement.src = article.image;
-
-        const nomElement = document.createElement("h2");
-        nomElement.innerText = article.nom;
-
-        const prixElement = document.createElement("p");
-        prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;   //Opérateur Ternaire
-
-        const categorieElement = document.createElement("p");
-        categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-
-        const descriptionElement = document.createElement("p");
-        descriptionElement.innerText = article.description ?? "(Pas de description pour le moment.)"; //Opérateur Nullish
-
-        const disponibleElement = document.createElement("p");
-        disponibleElement.innerText = article.disponibilité ? "En stock" : "Rupture de stock";
-
-
-        const avisBouton = document.createElement("button");
-        avisBouton.dataset.id = article.id;
-        avisBouton.textContent = "Afficher les avis";
-
-        // On rattache nos Elements à pieceElement (la balise article)
-        sectionFiches.appendChild(pieceElement)
-        pieceElement.appendChild(imageElement);
-        pieceElement.appendChild(nomElement);
-        pieceElement.appendChild(prixElement);
-        pieceElement.appendChild(categorieElement);
-        pieceElement.appendChild(descriptionElement);
-        pieceElement.appendChild(disponibleElement);
-
-        pieceElement.appendChild(avisBouton);
-
-        // Ajout de la fonction ajoutListenersAvis
-        ajoutListenersAvis();
-    }
+	}
+	ajoutListenersAvis();
 }
 
-//Première affichage de la page
-genererPieces(pieces)
+genererPieces(pieces);
 
+for (let i = 0; i < pieces.length; i++) {
+	const id = pieces[i].id;
+	const avisJSON = window.localStorage.getItem(`avis-piece-${id}`);
+	const avis = JSON.parse(avisJSON);
 
-//trier
-const boutonCroissant = document.querySelector(".btn-croissant");
-boutonCroissant.addEventListener("click", function () {
- 
-    const piecesCroissant = Array.from(pieces);
+	if (avis !== null) {
+		const pieceElement = document.querySelector(`article[data-id="${id}"]`);
+		afficherAvis(pieceElement, avis);
+	}
+}
 
-    piecesCroissant.sort(function(a,b){
-        return a.prix - b.prix
-    });
-    // Effacement de l'écran et regénération de la page
-    document.querySelector(".fiches").innerHTML = "";
-    genererPieces(piecesCroissant);
+//gestion des bouttons 
+const boutonTrier = document.querySelector(".btn-trier");
+
+boutonTrier.addEventListener("click", function () {
+	const piecesOrdonnees = Array.from(pieces);
+	piecesOrdonnees.sort(function (a, b) {
+		return a.prix - b.prix;
+	});
+	document.querySelector(".fiches").innerHTML = "";
+	genererPieces(piecesOrdonnees);
 });
 
-const boutonDecroissant = document.querySelector(".btn-decroissant");
-boutonDecroissant.addEventListener("click", function () {
- 
-    const piecesDecroissant = Array.from(pieces);
-
-    piecesDecroissant.sort(function(a,b){
-        return b.prix - a.prix
-    });
-    // Effacement de l'écran et regénération de la page
-    document.querySelector(".fiches").innerHTML = "";
-    genererPieces(piecesDecroissant);
-});
-
-
-//filter
 const boutonFiltrer = document.querySelector(".btn-filtrer");
+
 boutonFiltrer.addEventListener("click", function () {
-    const piecesFiltrees = pieces.filter(function(piece){
-        return piece.prix <=35;
-    })
-    // Effacement de l'écran et regénération de la page
-    document.querySelector(".fiches").innerHTML = "";
-    genererPieces(piecesFiltrees);
+	const piecesFiltrees = pieces.filter(function (piece) {
+		return piece.prix <= 35;
+	});
+	document.querySelector(".fiches").innerHTML = "";
+	genererPieces(piecesFiltrees);
 });
 
-const boutonFiltrer2 = document.querySelector(".btn-filtrer-2");
-boutonFiltrer2.addEventListener("click", function () {
-    const piecesFiltrees2 = pieces.filter(function(piece){
-        return piece.description;
-    })
-    // Effacement de l'écran et regénération de la page
-    document.querySelector(".fiches").innerHTML = "";
-    genererPieces(piecesFiltrees2);
+//Correction Exercice
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", function () {
+	const piecesOrdonnees = Array.from(pieces);
+	piecesOrdonnees.sort(function (a, b) {
+		return b.prix - a.prix;
+	});
+	document.querySelector(".fiches").innerHTML = "";
+	genererPieces(piecesOrdonnees);
 });
 
+const boutonNoDescription = document.querySelector(".btn-nodesc");
+
+boutonNoDescription.addEventListener("click", function () {
+	const piecesFiltrees = pieces.filter(function (piece) {
+		return piece.description;
+	});
+	document.querySelector(".fiches").innerHTML = "";
+	genererPieces(piecesFiltrees);
+});
 
 const noms = pieces.map(piece => piece.nom);
-for(let i = pieces.length -1 ; i >= 0; i--){
-   if(pieces[i].prix > 35){
-       noms.splice(i,1)
-   }
+for (let i = pieces.length - 1; i >= 0; i--) {
+	if (pieces[i].prix > 35) {
+		noms.splice(i, 1);
+	}
 }
+console.log(noms);
+//Création de l'en-tête
 
-
-//Création de la liste abordable
-const abordablesElements = document.createElement('ul');
-abordablesElements.innerText = "Pièces abordables :" 
+const pElement = document.createElement("p");
+pElement.innerText = "Pièces abordables";
+//Création de la liste
+const abordablesElements = document.createElement("ul");
 //Ajout de chaque nom à la liste
-for(let i=0; i < noms.length ; i++){
-   const nomElement = document.createElement('li');
-   nomElement.innerText = noms[i];
-   abordablesElements.appendChild(nomElement)
+for (let i = 0; i < noms.length; i++) {
+	const nomElement = document.createElement("li");
+	nomElement.innerText = noms[i];
+	abordablesElements.appendChild(nomElement);
 }
 // Ajout de l'en-tête puis de la liste au bloc résultats filtres
-document.querySelector('.abordables').appendChild(abordablesElements)
+document.querySelector(".abordables")
+	.appendChild(pElement)
+	.appendChild(abordablesElements);
 
+const nomsDisponibles = pieces.map(piece => piece.nom);
+const prixDisponibles = pieces.map(piece => piece.prix);
 
-
-const nomsDispo = pieces.map(piece => piece.nom);
-const prixDispo = pieces.map(piece => piece.prix);
-
-for(let i = pieces.length -1 ; i >= 0; i--){
-   if(pieces[i].disponibilité != true){
-       nomsDispo.splice(i,1)
-       prixDispo.splice(i,1)
-   }
+for (let i = pieces.length - 1; i >= 0; i--) {
+	if (pieces[i].disponibilite === false) {
+		nomsDisponibles.splice(i, 1);
+		prixDisponibles.splice(i, 1);
+	}
 }
 
-    //Création de la liste disponible
-const disponibleElements = document.createElement('ul');
-disponibleElements.innerText = "Pièces disponible :" 
+const disponiblesElement = document.createElement("ul");
 
-//Ajout de chaque nom à la liste
-for(let i=0; i < nomsDispo.length ; i++){
-   const nomDispoElement = document.createElement('li');
-   nomDispoElement.innerText = `${nomsDispo[i]} -- ${prixDispo[i]} €`;
-   disponibleElements.appendChild(nomDispoElement)
+for (let i = 0; i < nomsDisponibles.length; i++) {
+	const nomElement = document.createElement("li");
+	nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`;
+	disponiblesElement.appendChild(nomElement);
 }
-// Ajout de l'en-tête puis de la liste au bloc résultats filtres
-document.querySelector('.disponible').appendChild(disponibleElements)
 
+const pElementDisponible = document.createElement("p");
+pElementDisponible.innerText = "Pièces disponibles:";
+document.querySelector(".disponibles").appendChild(pElementDisponible).appendChild(disponiblesElement);
 
-const inputPrixMax = document.querySelector('#prix-max');
-inputPrixMax.addEventListener('input', function(){
-    const piecesFiltrees = pieces.filter(function(piece){
-        return piece.prix <= inputPrixMax.value
-    })
-    document.querySelector(".fiches").innerHTML = "";
-    genererPieces(piecesFiltrees);
-})
+const inputPrixMax = document.querySelector("#prix-max");
+inputPrixMax.addEventListener("input", function () {
+	const piecesFiltrees = pieces.filter(function (piece) {
+		return piece.prix <= inputPrixMax.value;
+	});
+	document.querySelector(".fiches").innerHTML = "";
+	genererPieces(piecesFiltrees);
+});
 
+// Ajout du listener pour mettre à jour des données du localStorage
+const boutonMettreAJour = document.querySelector(".btn-maj");
+boutonMettreAJour.addEventListener("click", function () {
+	window.localStorage.removeItem("pieces");
+});
+
+await afficherGraphiqueAvis();
